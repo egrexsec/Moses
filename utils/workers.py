d69602b24513b1dabc49a1c7b31005a3f91ea0d8
mell0wx <mell0wx@proton.me>
2026-05-29 09:25:49 -0400

    Add cancelled job skip logic to worker

--- utils/workers.py
+++ utils/workers.py
@@ -1,6 +1,8 @@
 import threading
 from queue import Queue
 
+from utils.process_registry import process_registry
+
 
 class BackgroundWorker:
     def __init__(self):
@@ -16,16 +18,24 @@ class BackgroundWorker:
     def add_task(self, func, *args, **kwargs):
         self.tasks.put((func, args, kwargs))
 
+    def should_skip_task(self, args, kwargs):
+        job_id = args[0] if args else kwargs.get("job_id")
+        return bool(job_id and process_registry.is_cancelled(job_id))
+
     def run(self):
         while self.running:
             func, args, kwargs = self.tasks.get()
 
             try:
-                func(*args, **kwargs)
+                if not self.should_skip_task(args, kwargs):
+                    func(*args, **kwargs)
             except Exception as e:
                 print(f"Worker Error: {e}")
+            finally:
+                self.tasks.task_done()
 
-            self.tasks.task_done()
+    def pending_count(self):
+        return self.tasks.qsize()
 
 
 background_worker = BackgroundWorker()
